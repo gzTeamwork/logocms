bce315046376f704479cf8a265735bbcbe4508f9
Zico郭 <21520993@qq.com>
2017-10-22 23:25:29 +0800

    0.0.2

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,12 +12,13 @@ Page({
       { name: 'vi_item', value: [], lebal: 'VI设计项', preText: '请选择您需要的VI设计项目', isMust: true, model: 'multi' },
       { name: 'logo_style', value: 1, lebal: '喜欢的Logo风格', preText: '请选择您喜爱的Logo设计风格', isMust: true, model: 'single' },
       { name: 'design_suggest', value: '', lebal: '设计要求', preText: '请详细描述您的设计要求，有助高效完成logo设计', isMust: false, model: 'textarea', },
+      { name: 'total_price', value: 0, noLebal: true, model: 'total_price' },
       { name: 'user_name', value: '', lebal: '您的姓名', preText: '请填写您的名字', isMust: true, model: 'input' },
       { name: 'user_phone', value: '', lebal: '您的电话', preText: '请填写您的联系方式(手机)', isMust: true, model: 'input' },
       { name: 'user_company', value: '', lebal: '公司名字', preText: '请填写您的公司名', isMust: true, model: 'input' },
     ],
+    setPrice: ['1680', '2680', '3680'],
     fromParams: {},
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -30,28 +31,48 @@ Page({
       fromParams[e.target.id] = e.detail.value
       this.setData({ fromParams: fromParams })
     }
+    //  通过轮询fromData,修改对应条目的value值,达到双向绑定
     var fromData = this.data.fromData
     for (let i = 0; i < fromData.length; i++) {
       if (fromData[i].name == e.target.id) {
         fromData[i].value = e.detail.value
       }
     }
+    if (e.target.id == 'design_set') {
+      fromData[5].value = this.data.setPrice[e.detail.value]
+      console.log(this.data.setPrice[e.detail.value - 1])
+    }
     this.setData({ fromData: fromData })
     console.log(e)
+    wx.pageScrollTo({
+      scrollTop: e.target.offsetTop - 60
+    })
+  },
+  //  VI项改变
+  viItemsChange(e) {
+    this.inputEvent(e);
+    console.log(fromModule)
+  },
+  //  提交Logo表单
+  bindFromSubmit() {
+    wx.showActionSheet({
+      itemList: this.data.fromData,
+      success: function (res) {
+        console.log(res.tapIndex)
+      },
+      fail: function (res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
+  //  直接联系拨打电话
+  bindContactUs() {
+    wx.makePhoneCall({
+      phoneNumber: '13828471634'
+    })
   },
   //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  bindtaptest: function () {
-    wx.switchTab({
-      url: '/pages/single/single'
-    })
-  },
   onLoad: function () {
-
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -80,7 +101,6 @@ Page({
     }
   },
   onShow() {
-    console.log(this.data.fromParams.logo_name)
   },
   getUserInfo: function (e) {
     console.log(e)
