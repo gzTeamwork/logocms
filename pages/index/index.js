1a042b3bcf64cf47afab4f715415d3d23b9bf8b3
Zico郭 <21520993@qq.com>
2017-10-24 16:01:43 +0800

    0.0.3

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,7 +17,6 @@ Page({
       { name: 'user_phone', value: '', lebal: '您的电话', preText: '请填写您的联系方式(手机)', isMust: true, model: 'input' },
       { name: 'user_company', value: '', lebal: '公司名字', preText: '请填写您的公司名', isMust: true, model: 'input' },
     ],
-    setPrice: ['1680', '2680', '3680'],
     fromParams: {},
     userInfo: {},
     hasUserInfo: false,
@@ -25,6 +24,7 @@ Page({
   },
   //  表单输入事件,同步数据到data
   inputEvent(e) {
+    //  计划废弃fromParams
     if (e.target.id.length > 1) {
       fromParams[e.target.id] = e.detail.value
       this.setData({ fromParams: fromParams })
@@ -36,10 +36,6 @@ Page({
         fromData[i].value = e.detail.value
       }
     }
-    if (e.target.id == 'design_set') {
-      fromData[5].value = this.data.setPrice[e.detail.value]
-      console.log(this.data.setPrice[e.detail.value - 1])
-    }
     this.setData({ fromData: fromData })
     console.log(e)
   },
@@ -50,42 +46,58 @@ Page({
     console.log(setVis);
     //  轮询套餐下的vi项,设置对应的tof
     for (let i = 0; i < viItems.length; i++) {
-      let key = findArray(setVis, viItems[i].name);
-      key > -1 ? console.log(viItems[i]) : ''
+      let key = findArray(setVis.vis, viItems[i].id);
+      // key > -1 ? console.log(viItems[i]) : ''
       viItems[i].checked = (key > -1);
+      viItems[i].disable = (key > -1)
     }
-
-
-    // for (let i = 0; i <= setVis.vis.length + 1; i++) {
-    //   let key = findArray(viItems, { id: setVis.vis[i] });
-    //   if (key !== -1) {
-    //     console.log(viItems[key])
-    //     viItems[key].checked = true
-    //   } else {
-    //     viItems[key].checked = false
-    //   }
-    // }
     console.log(viItems)
-    this.viItemsChange({ target: { id: 'vi_item' }, detail: { value: setViitems } })
+    var fromData = this.data.fromData
+    fromData[2].value = viItems;
+    fromData[5].value = designSetArray[e.detail.value].price
+    this.setData({ fromData: fromData })
   },
   //  VI项改变
   viItemsChange(e) {
-    // this.inputEvent(e);
-    // console.log(e.target.dataset.price)
-    // console.log(e.detail.value.length)
-    // let total = 0;
-    // for (let i = 0; i < e.detail.value.length; i++) {
-    //   var vi = findArray(viItems, { name: e.detail.value[i] })
-    //   total = total + parseInt(viItems[vi].price)
-    // }
-    // // 汇总报价
-    // var fromData = this.data.fromData
-    // fromData[5].value = total;
-    // this.setData({ fromData: fromData })
+    var newViItems = e.detail.value
+    var total_price = designSetArray[this.data.fromData[1].value].price
+    // 获取额外增加的vi项
+    for (let i = 0; i < viItems.length; i++) {
+      if (viItems[i].disable == false) {
+        if ((findArray(newViItems, viItems[i].name) > -1)) {
+          total_price = parseInt(total_price) + parseInt(viItems[i].price);
+          viItems[i].checked = true
+        } else {
+          viItems[i].checked = false
+        }
+      }
+    }
+    var fromData = this.data.fromData
+    fromData[2].value = viItems;
+    fromData[5].value = total_price
+    this.setData({ fromData: fromData })
   },
   //  提交Logo表单
   bindFromSubmit() {
-    console.log(this.data.fromData)
+    var fromData = this.data.fromData
+    var params = {}
+    params.userInfo = this.data.userInfo
+    for (let key in fromData) {
+      params[fromData[key].name] = fromData[key].value
+      console.log(params)
+    }
+    let serverUrl = 'http://www.diavision.cn/diavision/public/diavision/index/api'
+    wx.request({
+      url: serverUrl, //仅为示例，并非真实的接口地址
+      data: params[0],
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+
   },
   //  直接联系拨打电话
   bindContactUs() {
@@ -101,6 +113,9 @@ Page({
     var fromData = this.data.fromData
     fromData[2].value = viItems
     this.setData({ fromData: fromData })
+    this.designSetChange({
+      target: { id: 'design_set' }, detail: { value: 0 }
+    })
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -139,115 +154,31 @@ Page({
     })
   }
 })
-var designSetArray = [{ name: 'Logo设计套餐A', vis: [2, 3, 4] }, { name: 'Logo设计套餐B', vis: [2, 3, 4, 5, 6, 7] }, { name: 'VIS设计套餐', vis: [2, 3, 4, 5, 6, 7, 8, 17] }];
-var viItems = [{
-  id: '2',
-  name: '标志墨稿',
-  price: '200',
-  checked: true
-},
-{
-  id: '3',
-  name: '标志反白图',
-  price: '200',
-  checked: true
-},
-{
-  id: '4',
-  name: '标志标准制图',
-  price: '200',
-  checked: true
-},
-{
-  id: '5',
-  name: '标志方格制图',
-  price: '200',
-  checked: true
-},
-{
-  id: '6',
-  name: '标志比例限定',
-  price: '200',
-  checked: true
-},
-{
-  id: '7',
-  name: '标志色彩展示',
-  price: '200',
-  checked: true
-},
-{
-  id: '16',
-  name: '企业标准色',
-  price: '200',
-  checked: true
-},
-{
-  id: '17',
-  name: '辅助色系列',
-  price: '200',
-  checked: true
-},
-{
-  id: '#',
-  name: '辅助图形',
-  price: '300',
-  checked: true
-},
-{
-  id: '#',
-  name: '名片',
-  price: '300',
-  checked: true
-},
-{
-  id: '4',
-  name: '信封',
-  price: '200',
-  checked: true
-},
-{
-  id: '8',
-  name: '信纸',
-  price: '200',
-  checked: true
-},
-{
-  id: '#',
-  name: 'PPT模板',
-  price: '300',
-  checked: true
-},
-{
-  id: '#',
-  name: '工号牌',
-  price: '300',
-  checked: true
-},
-{
-  id: '#',
-  name: '工作证',
-  price: '300',
-  checked: true
-},
-{
-  id: '77',
-  name: '标识伞',
-  price: '300',
-  checked: true
-},
-{
-  id: '#',
-  name: '标识牌',
-  price: '300',
-  checked: true
-},
-{
-  id: '16',
-  name: '合同书范本',
-  price: 200,
-  checked: true
-}]
+var designSetArray = [
+  { name: 'Logo设计套餐A', vis: [2, 3, 4], price: 2380 },
+  { name: 'Logo设计套餐B', vis: [2, 3, 4, 5, 6, 7], price: 2680 },
+  { name: 'VIS设计套餐', vis: [2, 3, 4, 5, 6, 7, 8, 17], price: 3680 }
+];
+var viItems = [
+  { id: '2', name: '标志墨稿', price: 200, checked: true, disable: true },
+  { id: '3', name: '标志反白图', price: 200, checked: true, disable: true },
+  { id: '4', name: '标志标准制图', price: 200, checked: true, disable: true },
+  { id: '5', name: '标志方格制图', price: 200, checked: false, disable: false },
+  { id: '6', name: '标志比例限定', price: 200, checked: false, disable: false },
+  { id: '7', name: '标志色彩展示', price: 200, checked: false, disable: false },
+  { id: '16', name: '企业标准色', price: 200, checked: false, disable: false },
+  { id: '17', name: '辅助色系列', price: 200, checked: false, disable: false },
+  { id: '18', name: '辅助图形', price: 300, checked: false, disable: false },
+  { id: '19', name: '名片', price: 300, checked: false, disable: false },
+  { id: '24', name: '信封', price: 200, checked: true, disable: true },
+  { id: '8', name: '信纸', price: 200, checked: false, disable: false },
+  { id: '20', name: 'PPT模板', price: 300, checked: false, disable: false },
+  { id: '21', name: '工号牌', price: 300, checked: false, disable: false },
+  { id: '22', name: '工作证', price: 300, checked: false, disable: false },
+  { id: '77', name: '标识伞', price: 300, checked: false, disable: false },
+  { id: '23', name: '标识牌', price: 300, checked: false, disable: false },
+  { id: '16', name: '合同书范本', price: 200, checked: false, disable: false }
+]
 
 function findArray(array, feature, all = true) {
   for (let index in array) {
